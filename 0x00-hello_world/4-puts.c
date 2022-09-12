1874ddaff83c0e171d21476c6bc7541df3dfda71
Adebayo Abdulmaleek <imalyk.inc@gmail.com>
2022-09-12 13:52:29 -0700

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+include <stdio.h>
 
+/**
+*
+*Description: This program prints " Programming is like building a multilingual puzzle"
+* return 0
+*
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
